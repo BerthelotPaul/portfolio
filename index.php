d41f5c610c020c3d23c60b219a6ab02b8395e96a
BerthelotPaul <paulberthelotpro@gmail.com>
2022-01-04 15:13:18 +0100

    ajout de reset et style css , essai et reflexion sur le portfolio

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Paul Berthelot</title>
+    <link rel="stylesheet" href="./css/reset.css">
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+
+<body>
+
+    <div class="accueil">
+        <h1>Paul Berthelot</h1>
+        <h2>Je suis développeur Junior</h2>
+    </div>
+
+    <div class="titre">
+        <h2>A Propos</h2>
+    </div>
+
+    <div class="propos">
+    <div class="card">
+        <h2>Paul Berthelot</h2>
+        <h3>Saint-Malo , France</h3>
+        <img src="" alt="" class="logo">
+       
+    </div>
+
+    </div>
+
+
+    <!-- 
+        <footer><a href="">RGPD</a> -<a> Ver.0.1 </a>- <a>@PaulBerthelot</a></footer>
+
+    !-->
+</body>
+
+</html>
