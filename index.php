0417ecbbc7d95be5f4207ff12862ec790106ed3b
BerthelotPaul <paulberthelotpro@gmail.com>
2022-01-05 14:19:49 +0100

    navbar icon css

--- index.php
+++ index.php
@@ -6,34 +6,22 @@
     <title>Paul Berthelot</title>
     <link rel="stylesheet" href="./css/reset.css">
     <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/icon.css">
 </head>
 
 <body>
+    <div class="navbar">
+        <i class="gg-home-alt"></i>
+        <i class="gg-profile"></i>
+        <i class="gg-file-document"></i>
+        <i class="gg-work-alt"></i>
+        <i class="gg-mail"></i>
+    </div>
 
     <div class="accueil">
         <h1>Paul Berthelot</h1>
-        <h2>Je suis développeur Junior</h2>
+        <h2>Développeur Web Junior</h2>
     </div>
-
-    <div class="titre">
-        <h2>A Propos</h2>
-    </div>
-
-    <div class="propos">
-    <div class="card">
-        <h2>Paul Berthelot</h2>
-        <h3>Saint-Malo , France</h3>
-        <img src="" alt="" class="logo">
-       
-    </div>
-
-    </div>
-
-
-    <!-- 
-        <footer><a href="">RGPD</a> -<a> Ver.0.1 </a>- <a>@PaulBerthelot</a></footer>
-
-    !-->
 </body>
 
 </html>
