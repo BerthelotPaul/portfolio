f878cfb7a039ad115ef01ec7b8c4fa6eca2ce5ef
BerthelotPaul <paulberthelotpro@gmail.com>
2022-01-09 17:29:50 +0100

    update link

--- composants/profil.php
+++ composants/profil.php
@@ -11,8 +11,8 @@
 
 <body>
     <div class="navbar">
-        <a href="./index.php"><i class="gg-home-alt"></i></a>
-        <a href="./composants/profil.php"><i class="gg-profile"></i></a>
+        <a href="../index.php"><i class="gg-home-alt"></i></a>
+        <a href=""><i class="gg-profile"></i></a>
         <i class="gg-file-document"></i>
         <i class="gg-work-alt"></i>
         <i class="gg-mail"></i>
