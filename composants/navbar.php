ab43b8aaa6af9a87e5ad224e6d8bddeed3ac2777
Paul Berthelot <89459622+BerthelotPaul@users.noreply.github.com>
2022-03-08 11:11:22 +0100

    Delete composants directory

--- composants/navbar.php
+++
@@ -1,7 +0,0 @@
-<div class="navbar">
-    <a href="./index.php"><i class="gg-home-alt"></i></a>
-    <a href="./composants/profil.php"><i class="gg-profile"></i></a> 
-    <i class="gg-file-document"></i>
-    <i class="gg-work-alt"></i>
-    <i class="gg-mail"></i>
-</div>
